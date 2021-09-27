036a45d5ffdf2749095375dcf1a43cf59f112592
Hiroaki Sasaki <sasa.hiro0125@gmail.com>
2021-09-27 14:18:28 +0900

    fix next config

--- apps/medical/next.config.js
+++ apps/medical/next.config.js
@@ -2,4 +2,13 @@ const withTM = require("next-transpile-modules")(["../../libs/core"]);
 
 module.exports = withTM({
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "react/jsx-dev-runtime": "react/jsx-dev-runtime.js",
+      "react/jsx-runtime": "react/jsx-runtime.js",
+    }
+
+    return config;
+  }
 })
