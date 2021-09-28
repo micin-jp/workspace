4230bb635d6c0a8acb8a5c61d45237c37a41d990
Hiroaki Sasaki <sasa.hiro0125@gmail.com>
2021-09-28 13:18:42 +0900

    fix test

--- apps/medical/src/components/Button/Button.spec.tsx
+++ apps/medical/src/components/Button/Button.spec.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { Button } from './';
 
-describe('test', () => {
-    it('sample', () => {
+describe('Components/Button', () => {
+    it('renders correctly', () => {
         render(<Button />);
 
         const button = screen.getByText("アプリ内のボタンです");
