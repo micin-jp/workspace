08b3887e384f776ef42111e7b7117b5392b699e5
Hiroaki Sasaki <sasa.hiro0125@gmail.com>
2021-09-28 13:29:28 +0900

    rename libs component dir

--- apps/medical/pages/index.js
+++ apps/medical/pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 import { Button } from "~/components/Button";
-import { Button as SharedButton } from "~libs/core/components/Button"
+import { Button as SharedButton } from "~libs/shared/components/Button"
 
 export default function Home() {
   return (
